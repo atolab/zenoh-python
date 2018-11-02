9570304469e617c5d46358ec907caf3ae61ea1fe
Angelo Corsaro <angelo@icorsaro.net>
2018-11-02 22:30:12 +0100

    initial version of python's zenoh API

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+    name='zenoh',
+    version='0.1.0',
+    packages=['zenoh', ],
+    author='kydos',
+    url='https://github.com/atolab/zenoh-python',
+    install_requires=['jsonpickle']
+)
