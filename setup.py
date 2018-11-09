c18fdd1e0f214eff95b9a865e195f8f93f5eaa99
kydos <angelo@icorsaro.net>
2018-11-09 18:03:33 +0100

    added more info

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setup(
     name='zenoh',
-    version='0.1.0',
+    version='0.1.1',
     packages=['zenoh', ],
     author='kydos',
     description="Python client API for zenoh",
