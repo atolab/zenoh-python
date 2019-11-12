7282898b16b98539f974cc4d405c5fe00645c02d
Julien Enoch <946462+JEnoch@users.noreply.github.com>
2019-11-12 15:18:57 +0100

    Fix code style errors

--- zenoh/binding.py
+++ zenoh/binding.py
@@ -128,18 +128,20 @@ class z_timestamp_t(Structure):
                 ('time', c_size_t)]
 
     def __hash__(self):
-        return hash((self.time, self.clock_id[0], self.clock_id[1], self.clock_id[2], self.clock_id[3],
-            self.clock_id[4], self.clock_id[5], self.clock_id[6], self.clock_id[7],
-            self.clock_id[8], self.clock_id[9], self.clock_id[10], self.clock_id[11],
-            self.clock_id[12], self.clock_id[13], self.clock_id[14], self.clock_id[15]))
+        return hash((self.time, self.clock_id[0], self.clock_id[1],
+                     self.clock_id[2], self.clock_id[3], self.clock_id[4],
+                     self.clock_id[5], self.clock_id[6], self.clock_id[7],
+                     self.clock_id[8], self.clock_id[9], self.clock_id[10],
+                     self.clock_id[11], self.clock_id[12], self.clock_id[13],
+                     self.clock_id[14], self.clock_id[15]))
 
     def __eq__(self, other):
         if isinstance(other, self.__class__):
             if self.time != other.time:
-               return False
-            for i in range(0,15):
-               if self.clock_id[i] != other.clock_id[i]:
-                   return False
+                return False
+            for i in range(0, 15):
+                if self.clock_id[i] != other.clock_id[i]:
+                    return False
             return True
 
     def __lt__(self, other):
@@ -147,7 +149,7 @@ class z_timestamp_t(Structure):
             return True
         if self.time > other.time:
             return False
-        for i in range(0,15):
+        for i in range(0, 15):
             if self.clock_id[i] < other.clock_id[i]:
                 return True
             if self.clock_id[i] > other.clock_id[i]:
@@ -166,17 +168,19 @@ class z_timestamp_t(Structure):
         """
         Return the Timestamp's creation time as a float
         (i.e. number of seconds since Epoch:  January 1, 1970, 00:00:00 (UTC))
-        Warning: the time might be rounded, depending the float precision on your host.
+        Warning: the time might be rounded, depending the float precision on
+        your host.
         """
         sec = self.time >> 32
         frac = self.time & 0xffffffff
-        ns = float(frac) / 0x100000000    # number of NTP fraction per second (2^32)
-        return sec+ns
+        ns = float(frac) / 0x100000000
+        return sec + ns
 
     def datetime(self, tzinfo):
         """
         Return the Timestamp's creation time as a datetime.datetime
-        Warning: the time is rounded to milliseconds as datetime precision is millisecond.
+        Warning: the time is rounded to milliseconds as datetime precision
+        is millisecond.
         """
         return datetime.datetime.fromtimestamp(self.floattime(), tzinfo)
 
