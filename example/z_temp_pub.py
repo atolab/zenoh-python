abddec7d67fcbc9922ced1ac4ad5851b6725f0b6
Olivier Hécart <olivier.hecart@adlinktech.com>
2019-11-13 11:56:06 +0100

    Code style

--- example/z_temp_pub.py
+++ example/z_temp_pub.py
@@ -1,12 +1,14 @@
-from zenoh import Zenoh 
+from zenoh import Zenoh
 import random
 import time
 import sys
 
 random.seed()
 
+
 def read_temp():
-    return random.randint(15, 30)    
+    return random.randint(15, 30)
+
 
 def run_sensor_loop(z, pub):
     # read and produce e temperature every half a second
@@ -15,11 +17,12 @@ def run_sensor_loop(z, pub):
         z.stream_data(pub, str(t).encode())
         time.sleep(0.1)
 
+
 if __name__ == "__main__":
     locator = None
     if len(sys.argv) > 1:
         locator = sys.argv[1]
-        
+
     z = Zenoh.open(locator)
     pub = z.declare_publisher('/myhome/kitcken/temp')
     run_sensor_loop(z, pub)
--- example/z_temp_sub.py
+++ example/z_temp_sub.py
@@ -2,18 +2,21 @@ from zenoh import Zenoh, SubscriberMode
 import time
 import sys
 
+
 def listener(rname, data, info):
     print("{}: {}".format(rname, data.decode()))
-    
 
-if __name__ == "__main__":    
+
+if __name__ == "__main__":
     locator = None
     if len(sys.argv) > 1:
         locator = sys.argv[1]
-    
+
     z = Zenoh.open(locator)
-    sub = z.declare_subscriber('/myhome/kitcken/temp', SubscriberMode.push(), listener)
-    
+    sub = z.declare_subscriber('/myhome/kitcken/temp',
+                               SubscriberMode.push(),
+                               listener)
+
     # Listen for one minute and then exit
     time.sleep(120)
     z.undeclare_subscriber(sub)
