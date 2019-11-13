51e04376307871d83b83855da191feba1f5cd5b5
Angelo Corsaro <angelo@icorsaro.net>
2019-11-13 18:09:18 +0800

    added optional locator

--- example/z_temp_pub.py
+++ example/z_temp_pub.py
@@ -1,6 +1,7 @@
 from zenoh import Zenoh 
 import random
 import time
+import sys
 
 random.seed()
 
@@ -12,9 +13,13 @@ def run_sensor_loop(z, pub):
     while True:
         t = read_temp()
         z.stream_data(pub, str(t).encode())
-        time.sleep(0.5)
+        time.sleep(0.1)
 
-if __name__ == "__main__":        
-    z = Zenoh.open()
+if __name__ == "__main__":
+    locator = None
+    if len(sys.argv) > 1:
+        locator = sys.argv[1]
+        
+    z = Zenoh.open(locator)
     pub = z.declare_publisher('/myhome/kitcken/temp')
     run_sensor_loop(z, pub)
--- example/z_temp_sub.py
+++ example/z_temp_sub.py
@@ -1,15 +1,20 @@
 from zenoh import Zenoh, SubscriberMode
 import time
+import sys
 
 def listener(rname, data, info):
     print("{}: {}".format(rname, data.decode()))
     
 
-if __name__ == "__main__":        
-    z = Zenoh.open()
+if __name__ == "__main__":    
+    locator = None
+    if len(sys.argv) > 1:
+        locator = sys.argv[1]
+    
+    z = Zenoh.open(locator)
     sub = z.declare_subscriber('/myhome/kitcken/temp', SubscriberMode.push(), listener)
     
     # Listen for one minute and then exit
-    time.sleep(10)
+    time.sleep(120)
     z.undeclare_subscriber(sub)
     z.close()
