06bdec9d5d29cffe17c59d80906dc2bf78759c90
Ivan Paez <ivan.paez@adlinktech.com>
2020-01-15 14:56:12 +0100

    updated travis.sh script after yaks & zenoh merged

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -4,8 +4,10 @@ WD=$(pwd)
 cd $(mktemp -d)
 RD=$(pwd)
 curl -L -o zenohd https://github.com/atolab/atobin/raw/master/zenoh/unstable/ubuntu/16.04/zenohd
+curl -L -o zenoh-plugin-storages.cmxs https://github.com/atolab/atobin/raw/master/zenoh-storages/unstable/ubuntu/16.04/zenoh-plugin-storages.cmxs
 chmod +x $RD/zenohd
-$RD/zenohd --verbosity=debug > $RD/zenohd.out 2>&1 & echo $! > $RD/zenohd.pid
+chmod +x $RD/zenoh-plugin-storages.cmxs
+$RD/zenohd -P "$RD/zenoh-plugin-storages.cmxs -w" --verbosity=debug > $RD/zenohd.out 2>&1 & echo $! > $RD/zenohd.pid
 ZPID=$(<"$RD/zenohd.pid")
 sleep 2;
 cat $RD/zenohd.out
